1d95339fcdb4039f56ab81c8c9575be76c967e10
Avinaba Mazumdar <53753281+Avinaba-Mazumdar@users.noreply.github.com>
2021-05-10 17:19:09 +0530

    Update 005. Find the Union and Intersection of the two sorted arrays.cpp

--- Love Babbar's FINAL450 DSA Cracker Sheet/001-035  Array/005. Find the Union and Intersection of the two sorted arrays.cpp
+++ Love Babbar's FINAL450 DSA Cracker Sheet/001-035  Array/005. Find the Union and Intersection of the two sorted arrays.cpp
@@ -7,7 +7,7 @@ int main(){
      for (int i=0; i<n; i++) cin>>a[i];
      for (int i=0; i<m; i++) cin>>b[i];
 
-     int Union[m+n], Inter[(m>n)?n:m];
+     int Union[m+n], Inter[(m>n) ? n:m];
      for (int i=0; i<n; i++){
           for (int j=0; j<m; j++){
                if (a[i]==b[j]) Inter[i]=a[i];
