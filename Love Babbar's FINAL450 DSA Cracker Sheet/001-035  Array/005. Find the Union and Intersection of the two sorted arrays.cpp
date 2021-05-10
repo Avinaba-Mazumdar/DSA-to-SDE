bddb90fa3752cf3c5b570ca460eaa43581ff82ac
Avinaba Mazumdar <avinaba2@gmail.com>
2021-05-10 22:43:40 +0530

    commit

--- Love Babbar's FINAL450 DSA Cracker Sheet/001-035  Array/005. Find the Union and Intersection of the two sorted arrays.cpp
+++ Love Babbar's FINAL450 DSA Cracker Sheet/001-035  Array/005. Find the Union and Intersection of the two sorted arrays.cpp
@@ -8,15 +8,20 @@ int main(){
      for (int i=0; i<m; i++) cin>>b[i];
 
      int Union[m+n], Inter[(m>n) ? n:m];
+
+      //Union
+      
+
+     //Intersection
      for (int i=0; i<n; i++){
           for (int j=0; j<m; j++){
                if (a[i]==b[j]) Inter[i]=a[i];
-               
           }
      }
      for(int i=0; i<((m>n)?n:m); i++){
            cout<<Inter[i]<<" ";
      }
+     //THE END
      return 0;
 }
 
