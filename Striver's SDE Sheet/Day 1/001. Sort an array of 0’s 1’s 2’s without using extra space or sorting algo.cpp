2144506e0f0343b2a5602b01d0fa65283b7c8523
Avinaba Mazumdar <avinaba2@gmail.com>
2021-05-14 22:38:28 +0530

    Working on Striver's SDE Sheet Day 1

---
+++ Striver's SDE Sheet/Day 1/001. Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n; cin>>n;
+      int a[n]; for(int i=0; i<n; i++) cin>>a[i];
+      int l=0, m=0, h=n-1;
+      for(int i=0; i<n; i++){
+            if(a[m] == 0) swap(a[l++], a[m++]);
+            if(a[m] == 1) m++;
+            if(a[m] == 2) swap(a[h--], a[m++]);
+      }
+}
