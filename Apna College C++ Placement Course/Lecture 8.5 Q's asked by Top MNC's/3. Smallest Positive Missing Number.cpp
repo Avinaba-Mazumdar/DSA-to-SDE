471075d7de0f7dbb41ff3caa031ff079a9c5f83b
Avinaba Mazumdar <avinaba2@gmail.com>
2021-05-20 12:07:55 +0530

    3

--- Apna College C++ Placement Course/Lecture 8.5 Q's asked by Top MNC's/3. Smallest Positive Missing Number.cpp
+++ Apna College C++ Placement Course/Lecture 8.5 Q's asked by Top MNC's/3. Smallest Positive Missing Number.cpp
@@ -5,14 +5,17 @@ using namespace std;
 
 int main(){
       int n, m=0; cin>>n;
-      int a[n]; for(int i=0; i<n; i++) cin>>a[i];
+      int a[n]; for(int i=0; i<n; i++){
+            cin>>a[i];
+            m = max(m, a[i]);
+      }
       bool b[m]; for(int i=0; i<m; i++) b[i] = false;
 
       for(int i=0; i<n; i++){
             if(b[a[i]] >= 0)
                   b[a[i]] = true;
       }
-      for(int i=0; i<n; i++){
+      for(int i=0; i<m; i++){
             if(b[a[i]] == false){
                   cout<<i<<endl;
                   break;
