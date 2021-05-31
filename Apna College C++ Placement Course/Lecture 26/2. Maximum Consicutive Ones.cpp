5bfb2a1ccc0585a928e75cf3dd3b82aad98d62b3
Avinaba Mazumdar <avinaba2@gmail.com>
2021-05-31 19:32:21 +0530

    26.2

---
+++ Apna College C++ Placement Course/Lecture 26/2. Maximum Consicutive Ones.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n,k; cin>>n>>k;
+      vector<int> a(n);
+      for(auto &i:a) cin>>i;
+
+      int i=0, zerocnt=0, ans=0;
+      for(int j=0;j<n; j++){
+            if(a[j] == 0) zerocnt++;
+            while(zerocnt > k){
+                  if(a[i] == 0) zerocnt--;
+                  i++;
+            }
+            ans = max(ans, j-i+1);
+      }
+
+      cout<<ans;
+
+      return 0;
+}
