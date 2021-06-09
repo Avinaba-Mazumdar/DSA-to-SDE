296d0ca30e1b79b4b010cc61de5ae9bb16eb183f
Avinaba Mazumdar <avinaba2@gmail.com>
2021-06-09 19:00:38 +0530

    2

---
+++ Striver's SDE Problems/Day 9 ▶ Recursion/3. Combination sum-1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void combList(int ind, vector<vector<int>> &vv, vector<int> &v, vector<int> A, int B){
+        if(B == 0){
+            vv.push_back(v);
+            return ;
+        }
+        if(ind == A.size()) return ;
+        
+        if(B >= A[ind]){
+            v.push_back(A[ind]);
+            combList(ind, vv, v, A, B-A[ind]);
+            v.pop_back();
+        }
+        combList(ind+1, vv, v, A, B);
+    }
+
+int main(){
+      int n, target; cin>>n>>target;
+      vector<int> a(n); for(int i=0; i<n; i++) cin>>a[i];
+      
+      vector<vector<int>> vv;
+      vector<int> v;
+      combList(0, vv, v, a, target);
+      for(int i=0; i<vv.size(); i++){
+            for(int j=0; j<vv[i].size(); j++) cout<<vv[i][j]<<" ";
+            cout<<endl;
+      }
+      return 0;
+}
