d1a590bb712efd0e4783d02ee0381fa58e7afa68
Avinaba-Mazumdar <avinaba2@gmail.com>
2021-07-13 16:04:50 +0530

    332

---
+++ Love Babbar's FINAL450 DSA Cracker Sheet/329-372  Graph/332. Create a Graph, print it.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n,e; cin>>n>>e;
+      vector<int> adj[n+1];
+
+      for(int i=0; i<e; i++){
+            int x,y; cin>>x>>y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+      }
+
+      for(int i=1; i<=n; i++){
+            cout<<i<<"--> ";
+            for(int j=0; j<adj[i].size(); j++)
+                  cout<<adj[i][j]<<" ";
+            cout<<endl;
+      }
+}
